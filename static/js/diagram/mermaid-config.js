5f43a44ca723a33e6aef681677e1d19c86569325
Thomas Weitzel <tweitzel@synformation.com>
2023-09-29 14:45:16 +0200

    add Mermaid to theme (via shortcode)

---
+++ static/js/diagram/mermaid-config.js
@@ -0,0 +1,7 @@
+// ATTENTION: if you change this file, make sure to re-calculate the integrity hash used in base.html
+document.addEventListener("DOMContentLoaded", function() {
+  mermaid.initialize({
+    startOnLoad: true,
+    logLevel: "error",
+  });
+});
