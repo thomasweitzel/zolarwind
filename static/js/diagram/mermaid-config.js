dbeed96e4ebb04c313f12c3eecd3fbccf8b58880
Thomas Weitzel <tweitzel@synformation.com>
2026-01-13 14:53:02 +0100

    remove misleading comment about recalculating the integrity hash because we use `get_hash(...)`

--- static/js/diagram/mermaid-config.js
+++ static/js/diagram/mermaid-config.js
@@ -1,4 +1,3 @@
-// ATTENTION: if you change this file, make sure to re-calculate the integrity hash used in base.html
 document.addEventListener("DOMContentLoaded", function() {
   mermaid.initialize({
     startOnLoad: true,
